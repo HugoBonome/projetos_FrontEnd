1688d0b7f84847e19155ed30604097060816d145
HugoBonome <143456036+HugoBonome@users.noreply.github.com>
2024-04-12 20:24:33 -0300

    Add files via upload

---
+++ atv.fe1.JavaScript/conceito.js
@@ -0,0 +1,33 @@
+
+function principal() {
+    let turma = [];
+
+    let nomes = ["Rick", "Morty"];
+
+    let peso = [1, 2, 3, 4];
+
+    let a1 = [10, 3];
+    let a2 = [10, 3];
+    let a3 = [10, 3];
+    let a4 = [10, 3];
+
+    turma[0] = nomes;
+    turma[1] = peso;
+    turma[2] = a1;
+    turma[3] = a2;
+    turma[4] = a3;
+    turma[5] = a4;
+    
+    let medias = calculaMedias(turma);
+}
+
+function calculaMedias(turma) {
+    
+    for (let i = 2; i< turma.length; i++){
+        
+        let nota[i] = (a1[i]*2 + a2[i]* + a3[i]*2 + a3[i]*3 + a4[i]*3)/10
+
+
+    }
+
+}
